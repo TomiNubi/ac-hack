bacbd0ee61fca753c95034f3dc14bdadb758c7aa
TomiNubi <tomisin.ogunnubi2020@gmail.com>
2022-11-12 21:06:35 +0000

    create signup page

---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class signup extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+  render() {
+    return (
+      <div>signup</div>
+    )
+  }
+}

6098bcbe7431af8c7ccdebe636be569b2daf5bed
TomiNubi <tomisin.ogunnubi2020@gmail.com>
2022-11-13 09:58:25 +0000

    improve signup

--- pages/signup.js
+++ pages/signup.js
@@ -3,34 +3,54 @@ import { ref, set } from "firebase/database";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 import {db, auth, app} from "../firebase-config"
 import { emailCheck } from '../Backend/email checker';
+import { withRouter } from 'next/router'
+import Link from 'next/link';
+
+
 
 export default class Signup extends Component {
 
-    constructor(props){
-        super(props);
+    constructor({router}){
+        super();
         this.state = {userProfile: {
             'email' : 'user7@sample.com',
             'password' : 'samplepassword',
             'type' : 'business',
             'points' : 0,
             'QR code': 'sample stuff',
+            
 
-        }, auth : auth, db: db, count: 10};
+        }, auth : auth, db: db, count: 10, loading: true,
+            type : "customer"};
     }
+   
+    componentDidMount(){
+      //  this.setState({user: auth.currentUser});
+        onAuthStateChanged(this.state.auth, (user) => {
+            this.setState({user: user}, () => {
+                this.setState({loading: false});
+                console.log(user)
+            });
+    })
 
-
+}
     initialiseUserProfile = (user, profile) => {
         console.log("initialise profile called")
         profile = {
             'name': this.state.name,
             'points' : 0
         }
+        console.log(this.state.type)
         
         set(ref(this.state.db, `users/${this.state.type}/${user.uid}/`) , profile).then(() => {
             console.log("Updated profile")
         }).catch((e) => console.error(e))
 
     }
+
+
+
+     
 
     signUpWithEmailAndPassword = () => {
         const email = this.state.email;
@@ -42,7 +62,7 @@ export default class Signup extends Component {
                 const user = userCredential.user;
                 this.initialiseUserProfile(user, profile); 
                 console.log("signed up") 
-                this.setState({user: this.state.auth.currentUser})
+                this.setState({user: user})
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -74,31 +94,32 @@ export default class Signup extends Component {
       }
       
       setType = (e) => {
-  
+        console.log("Dropdown value " + e.target.value)
         this.setState({type:e.target.value})
   
       }
 
   render() {
-    return (
+    return this.state.loading? <></> : this.state.user? <div><p>Welcome </p> <Link href='/customer'> Let's go </Link></div> :
+     (
       <div>
 
-        <form class="cream" action="home">
-        <input type="text" class="form_input" name="name" onChange={this.setName} autofocus placeholder='Name'/><br/><br/>
-            <input type="text" class="form_input" name="username" onChange={this.setEmail} autofocus placeholder='Username'/><br/><br/>
-            <input type="password" class="form_inputP" name="pword" onChange={this.setPassword} autofocus placeholder='Password' /><br/><br/>
+    <div class="cream" >
+        <input type="text" className="form_input" name="name" onChange={this.setName} autofocus placeholder='Name'/><br/><br/>
+            <input type="text" className="form_input" name="username" onChange={this.setEmail} autofocus placeholder='Username'/><br/><br/>
+            <input type="password" className="form_inputP" name="pword" onChange={this.setPassword} autofocus placeholder='Password' /><br/><br/>
             <div>
-            <select onChange={this.setType} name="type">
+            <select value={this.state.type}  onChange={this.setType} name="type">
                 <option value="customer">Customer</option>
                 <option value="business">Business</option>
                 <option value="supplier">Supplier</option>
             </select>
             </div>
           
-            <p class="forgot_password">Forgot Password</p>
-            <p class="bussiness">Business</p>
+            <p className="forgot_password">Forgot Password</p>
+            <p className="bussiness">Business</p>
             <button class="form_submit" value="Sign In" onClick={this.signUpWithEmailAndPassword}> Sign up </button>
-      </form> 
+      </div> 
         
         <button onClick={this.signUpWithEmailAndPassword}>
             signup
@@ -109,5 +130,5 @@ export default class Signup extends Component {
         </button>
       </div>
     )
-  }
+    }
 }

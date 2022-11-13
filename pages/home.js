a3c0c0c32565a0b40f6310c1be16e1dc622d677a
Dania Biniam <dania.biniam20@imperial.ac.uk>
2022-11-13 04:13:41 +0000

    removing splash.js

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <div>home</div>
+      <div>
+        <header id = "main-header">
+          <button color={"MediumSeaGreen"}>Points</button>
+          <button color={"green"}>Reedem</button>
+      </header>
+        <header>
+          <h1>
+            {"Your points..."}</h1>
+            </header>
+            <div className=" something">
+              <button onClick={() => {
+                navigate("/QR")
+              }} color= 'white'> QR Access </button>
+              </div>home</div>
     )
   }
 }

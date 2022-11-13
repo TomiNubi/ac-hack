129b8859702f7f79ddb17a5c00d34feeeef482d9
TomiNubi <tomisin.ogunnubi2020@gmail.com>
2022-11-13 09:58:43 +0000

    add sign up link

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../firebase-config"
-
+import Link from 'next/link';
 
 
 export default class Login extends Component {
@@ -35,7 +35,7 @@ export default class Login extends Component {
 
     setEmail = (e) => {
 
-      this.setState({email:e.target.value})
+      this.setState({email : e.target.value})
 
     }
 
@@ -53,11 +53,13 @@ export default class Login extends Component {
         <form class="cream" action="home">
         <input type="text" class="form_input" name="username" onChange={this.setEmail} autofocus placeholder='Username'/><br/><br/>
         <input type="password" class="form_inputP" name="pword" onChange={this.setPassword} autofocus placeholder='Password' /><br/><br/>
-        <input type="checkbox" class="remember_me" name="rememberme"></input>
-        <p class="remember_me_text">Remember Me</p>
+       
         <p class="forgot_password">Forgot Password</p>
-        <p class="bussiness">Bussiness</p>
-        <input class="form_submit" type="submit" value="Sign In"/>
+        <div>
+        <p class="forgot_password" >Dont have an account? Sign up <Link href="/signup"> here </Link></p>
+        </div>
+
+        <input class="form_submit" value="Sign In"/>
       </form> 
       
 
